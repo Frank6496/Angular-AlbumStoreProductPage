d5084dd9e228dd1cdbbcc778eeb42d6562a775b4
Frank6496 <francismuthoni254@gmail.com>
2020-10-10 20:08:07 +0300

    /

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { RouterModule, Routes } from '@angular/router';
+
 /*IMPORTING PRODUCTSERVICE IN THE APP MODULE*/
 import {ProductService} from './product.service';
 
@@ -11,6 +13,12 @@ import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { constants } from 'os';
+
+declare const appRoutes: Routes[{
+  path: products,
+  component: ProductListComponent
+}];
 
 @NgModule({
   declarations: [
@@ -23,7 +31,8 @@ import { ProductListComponent } from './product-list/product-list.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   /*ADDING PRODUCTSERVICE AS A PROVIDER TO THE ARRAYS OF PROVIDERS*/
   providers: [ProductService],

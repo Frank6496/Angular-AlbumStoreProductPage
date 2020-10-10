2eac469618ab4564393a5b722eb7d9656cbd1de7
Frank6496 <francismuthoni254@gmail.com>
2020-10-10 20:22:47 +0300

    completed module 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,8 @@ import { constants } from 'os';
 
 const appRoutes: Routes = [
   { path: 'products', component: ProductListComponent },
-  { path: 'product/:id', component: ProductListComponent }
+  { path: 'product/:id', component: ProductPageComponent },
+  { path: ' ', redirectTo: 'products', pathMatch: 'full' }
 ];
 
 @NgModule({
